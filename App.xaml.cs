9f5ca983ed721b45d96d1d83cea51dd45e5e06a9
Ammu1492 <60737792+Ammu1492@users.noreply.github.com>
2023-02-28 19:31:05 +0530

    Added a rating control sample

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIExample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTIxOTIzOEAzMjMwMmUzNDJlMzBBOXdpRFlrQThlenBkYjFON1N2cEFOSzhSeFVNb1k0ZUtsZ0RiWG1LRXFRPQ==");
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
